2e993e4a93d690ce1792f5b91597ce9880df0df8
Raphael Sander <raphael.sander75@gmail.com>
2023-09-30 18:08:58 -0300

    feat: connect api to mongodb

--- api/main.py
+++ api/main.py
@@ -1,46 +1,43 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
-import httpx
-import json
+from pymongo import MongoClient
+import os
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-class MonsterInfo(BaseModel):
-    id: int
-    name: str
-    health: int
+mongodb_url = os.environ['MONGODB_URL']
 
-async def fetch_monster_data(monster_id):
-    url = f"https://www.divine-pride.net/api/database/Monster/{monster_id}?apiKey=d8a0fbd9a7f4dfd9d330170bcfb2bfee"
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-        if response.status_code == 200:
-            monster_data = response.json()
-            return monster_data
-        else:
-            return None
+mongo_client = MongoClient(mongodb_url)
+db = mongo_client["ragnarok"]
+collection = db["monsters"]
 
-@app.get("/monster/{monster_id}")
-async def read_item(request: Request, monster_id: int):
-    monster_data = await fetch_monster_data(monster_id)
-    if monster_data:
-        monster_info = MonsterInfo(id=monster_data["id"], name=monster_data["name"], health=monster_data["stats"]["health"])
-    else:
-        monster_info = MonsterInfo(name="Monster Not Found", health=0)
-    return templates.TemplateResponse("monster_info.html", {"request": request, "monster_info": monster_info})
 
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
-@app.get("/data")
-async def get_data():
-    try:
-        with open("templates/data.json", "r") as file:
-            data = json.load(file)
-        return JSONResponse(content=data)
-    except FileNotFoundError:
-        return JSONResponse(content={"error": "Arquivo não encontrado"}, status_code=404)
+
+@app.get("/monster")
+async def monster(request: Request):
+    return templates.TemplateResponse("monster_info.html", {"request": request})
+
+
+@app.get("/api/monsters", response_model=list[dict])
+async def get_monsters():
+    monsters = collection.find({}, {"_id": 0, "name": 1, "id": 1})
+    monsters = [monster for monster in monsters]
+    return JSONResponse(content=monsters)
+
+
+@app.get("/api/monster/{monster_id}")
+async def get_monster(monster_id: int):
+    monster = collection.find_one({"id": monster_id}, {"_id": 0})
+
+    if monster:
+        monster["image_url"] = f"https://static.divine-pride.net/images/mobs/png/{monster['id']}.png"
+        return JSONResponse(content=monster)
+    else:
+        return JSONResponse(content={"error": f"{monster['name']} not found"}, status_code=404)
