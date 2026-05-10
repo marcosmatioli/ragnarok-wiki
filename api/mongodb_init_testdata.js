a098e5532714ccfc152fc39ff2258d51a6f13a8e
Raphael Sander <raphael.sander75@gmail.com>
2026-05-09 22:22:17 -0300

    feat: improvement on unit test

--- api/mongodb_init_testdata.js
+++ api/mongodb_init_testdata.js
@@ -7,40 +7,192 @@ if (!db.getCollectionNames().includes("monsters")) {
         "id": 1001,
         "dbname": "SCORPION",
         "drops": [
-            {"itemId": 990, "chance": 35, "stealProtected": false, "serverTypeName": null, "optionGroup": "G0"},
-            {"itemId": 904, "chance": 2750, "stealProtected": false, "serverTypeName": null, "optionGroup": "G0"},
-            {"itemId": 757, "chance": 29, "stealProtected": false, "serverTypeName": null, "optionGroup": "G0"},
-            {"itemId": 943, "chance": 105, "stealProtected": false, "serverTypeName": null, "optionGroup": "G0"},
-            {"itemId": 601, "chance": 500, "stealProtected": false, "serverTypeName": null, "optionGroup": "G0"},
-            {"itemId": 7041, "chance": 500, "stealProtected": false, "serverTypeName": null, "optionGroup": "G0"},
-            {"itemId": 625, "chance": 10, "stealProtected": false, "serverTypeName": null, "optionGroup": "G0"},
-            {"itemId": 4068, "chance": 1, "stealProtected": true, "serverTypeName": null, "optionGroup": "G0"}
+            {
+                "itemId": 990,
+                "chance": 35,
+                "stealProtected": false,
+                "serverTypeName": null,
+                "optionGroup": "G0"
+            },
+            {
+                "itemId": 904,
+                "chance": 2750,
+                "stealProtected": false,
+                "serverTypeName": null,
+                "optionGroup": "G0"
+            },
+            {
+                "itemId": 757,
+                "chance": 29,
+                "stealProtected": false,
+                "serverTypeName": null,
+                "optionGroup": "G0"
+            },
+            {
+                "itemId": 943,
+                "chance": 105,
+                "stealProtected": false,
+                "serverTypeName": null,
+                "optionGroup": "G0"
+            },
+            {
+                "itemId": 601,
+                "chance": 500,
+                "stealProtected": false,
+                "serverTypeName": null,
+                "optionGroup": "G0"
+            },
+            {
+                "itemId": 7041,
+                "chance": 500,
+                "stealProtected": false,
+                "serverTypeName": null,
+                "optionGroup": "G0"
+            },
+            {
+                "itemId": 625,
+                "chance": 10,
+                "stealProtected": false,
+                "serverTypeName": null,
+                "optionGroup": "G0"
+            },
+            {
+                "itemId": 4068,
+                "chance": 1,
+                "stealProtected": true,
+                "serverTypeName": null,
+                "optionGroup": "G0"
+            }
         ],
         "mapDrops": [],
         "metamorphosis": [],
         "mvpdrops": [],
         "name": "Escorpião",
         "propertyTable": {
-            "0": 100, "1": 150, "2": 90, "3": 25, "4": 100, "5": 125, "6": 100, "7": 100, "8": 100, "9": 100
+            "0": 100,
+            "1": 150,
+            "2": 90,
+            "3": 25,
+            "4": 100,
+            "5": 125,
+            "6": 100,
+            "7": 100,
+            "8": 100,
+            "9": 100
         },
-        "questObjective": [7131, 7493],
+        "questObjective": [
+            7131,
+            7493
+        ],
         "skill": [
-            {"idx": 22596, "skillId": 186, "status": "BERSERK_ST", "level": 1, "chance": 200, "casttime": 0, "delay": 5000, "interruptable": true, "changeTo": null, "condition": null, "conditionValue": null, "sendType": null, "sendValue": null},
-            {"idx": 22597, "skillId": 176, "status": "BERSERK_ST", "level": 1, "chance": 50, "casttime": 800, "delay": 5000, "interruptable": false, "changeTo": null, "condition": null, "conditionValue": null, "sendType": null, "sendValue": null},
-            {"idx": 22598, "skillId": 188, "status": "BERSERK_ST", "level": 1, "chance": 50, "casttime": 300, "delay": 5000, "interruptable": false, "changeTo": null, "condition": null, "conditionValue": null, "sendType": null, "sendValue": null}
+            {
+                "idx": 22596,
+                "skillId": 186,
+                "status": "BERSERK_ST",
+                "level": 1,
+                "chance": 200,
+                "casttime": 0,
+                "delay": 5000,
+                "interruptable": true,
+                "changeTo": null,
+                "condition": null,
+                "conditionValue": null,
+                "sendType": null,
+                "sendValue": null
+            },
+            {
+                "idx": 22597,
+                "skillId": 176,
+                "status": "BERSERK_ST",
+                "level": 1,
+                "chance": 50,
+                "casttime": 800,
+                "delay": 5000,
+                "interruptable": false,
+                "changeTo": null,
+                "condition": null,
+                "conditionValue": null,
+                "sendType": null,
+                "sendValue": null
+            },
+            {
+                "idx": 22598,
+                "skillId": 188,
+                "status": "BERSERK_ST",
+                "level": 1,
+                "chance": 50,
+                "casttime": 300,
+                "delay": 5000,
+                "interruptable": false,
+                "changeTo": null,
+                "condition": null,
+                "conditionValue": null,
+                "sendType": null,
+                "sendValue": null
+            }
         ],
         "slaves": [],
-        "sounds": ["monster_shell.wav", "scorpion_attack.wav", "scorpion_damage.wav", "scorpion_die.wav"],
+        "sounds": [
+            "monster_shell.wav",
+            "scorpion_attack.wav",
+            "scorpion_damage.wav",
+            "scorpion_die.wav"
+        ],
         "spawn": [
-            {"mapname": "moc_fild11", "amount": 41, "respawnTime": 5000},
-            {"mapname": "moc_fild18", "amount": 160, "respawnTime": 5000}
+            {
+                "mapname": "moc_fild11",
+                "amount": 41,
+                "respawnTime": 5000
+            },
+            {
+                "mapname": "moc_fild18",
+                "amount": 160,
+                "respawnTime": 5000
+            }
         ],
         "spawnSet": [],
         "stats": {
-            "attackRange": 1, "level": 16, "health": 136, "sp": 1, "str": 12, "int": 5, "vit": 10, "dex": 19, "agi": 15, "luk": 5, "rechargeTime": 1564, "atk1": 7, "atk2": 7,
-            "attack": {"minimum": 33, "maximum": 36}, "magicAttack": {"minimum": 25, "maximum": 30}, "defense": 16, "baseExperience": 169, "jobExperience": 115,
-            "aggroRange": 10, "escapeRange": 12, "movementSpeed": 200, "attackSpeed": 864, "attackedSpeed": 576, "element": 23, "scale": 0, "race": 4,
-            "magicDefense": 5, "hit": 231, "flee": 205, "ai": "MONSTER_TYPE_01", "mvp": 0, "class": 0, "attr": 0, "res": 0, "mres": 0
+            "attackRange": 1,
+            "level": 16,
+            "health": 136,
+            "sp": 1,
+            "str": 12,
+            "int": 5,
+            "vit": 10,
+            "dex": 19,
+            "agi": 15,
+            "luk": 5,
+            "rechargeTime": 1564,
+            "atk1": 7,
+            "atk2": 7,
+            "attack": {
+                "minimum": 33,
+                "maximum": 36
+            },
+            "magicAttack": {
+                "minimum": 25,
+                "maximum": 30
+            },
+            "defense": 16,
+            "baseExperience": 169,
+            "jobExperience": 115,
+            "aggroRange": 10,
+            "escapeRange": 12,
+            "movementSpeed": 200,
+            "attackSpeed": 864,
+            "attackedSpeed": 576,
+            "element": 23,
+            "scale": 0,
+            "race": 4,
+            "magicDefense": 5,
+            "hit": 231,
+            "flee": 205,
+            "ai": "MONSTER_TYPE_01",
+            "mvp": 0,
+            "class": 0,
+            "attr": 0,
+            "res": 0,
+            "mres": 0
         }
     });
 
@@ -48,14 +200,62 @@ if (!db.getCollectionNames().includes("monsters")) {
         "id": 1168,
         "dbname": "SCORPION_KING",
         "drops": [
-            {"itemId": 994, "chance": 23, "stealProtected": false, "serverTypeName": null, "optionGroup": "G0"},
-            {"itemId": 1046, "chance": 2425, "stealProtected": false, "serverTypeName": null, "optionGroup": "G0"},
-            {"itemId": 1005, "chance": 8, "stealProtected": false, "serverTypeName": null, "optionGroup": "G0"},
-            {"itemId": 904, "chance": 2500, "stealProtected": false, "serverTypeName": null, "optionGroup": "G0"},
-            {"itemId": 943, "chance": 1500, "stealProtected": false, "serverTypeName": null, "optionGroup": "G0"},
-            {"itemId": 509, "chance": 350, "stealProtected": false, "serverTypeName": null, "optionGroup": "G0"},
-            {"itemId": 512, "chance": 0, "stealProtected": false, "serverTypeName": null, "optionGroup": "G0"},
-            {"itemId": 4130, "chance": 1, "stealProtected": true, "serverTypeName": null, "optionGroup": "G0"}
+            {
+                "itemId": 994,
+                "chance": 23,
+                "stealProtected": false,
+                "serverTypeName": null,
+                "optionGroup": "G0"
+            },
+            {
+                "itemId": 1046,
+                "chance": 2425,
+                "stealProtected": false,
+                "serverTypeName": null,
+                "optionGroup": "G0"
+            },
+            {
+                "itemId": 1005,
+                "chance": 8,
+                "stealProtected": false,
+                "serverTypeName": null,
+                "optionGroup": "G0"
+            },
+            {
+                "itemId": 904,
+                "chance": 2500,
+                "stealProtected": false,
+                "serverTypeName": null,
+                "optionGroup": "G0"
+            },
+            {
+                "itemId": 943,
+                "chance": 1500,
+                "stealProtected": false,
+                "serverTypeName": null,
+                "optionGroup": "G0"
+            },
+            {
+                "itemId": 509,
+                "chance": 350,
+                "stealProtected": false,
+                "serverTypeName": null,
+                "optionGroup": "G0"
+            },
+            {
+                "itemId": 512,
+                "chance": 0,
+                "stealProtected": false,
+                "serverTypeName": null,
+                "optionGroup": "G0"
+            },
+            {
+                "itemId": 4130,
+                "chance": 1,
+                "stealProtected": true,
+                "serverTypeName": null,
+                "optionGroup": "G0"
+            }
         ],
         "mapDrops": [],
         "metamorphosis": [],
@@ -76,7 +276,12 @@ if (!db.getCollectionNames().includes("monsters")) {
         "questObjective": [],
         "skill": [],
         "slaves": [],
-        "sounds": ["monster_shell.wav", "peco_egg_heartbeat.wav", "pupa_die.wav", "pupa_split.wav"],
+        "sounds": [
+            "monster_shell.wav",
+            "peco_egg_heartbeat.wav",
+            "pupa_die.wav",
+            "pupa_split.wav"
+        ],
         "spawn": [],
         "spawnSet": [],
         "stats": {
@@ -93,8 +298,14 @@ if (!db.getCollectionNames().includes("monsters")) {
             "rechargeTime": 1700,
             "atk1": 630,
             "atk2": 113,
-            "attack": {"minimum": 555, "maximum": 807},
-            "magicAttack": {"minimum": 130, "maximum": 197},
+            "attack": {
+                "minimum": 555,
+                "maximum": 807
+            },
+            "magicAttack": {
+                "minimum": 130,
+                "maximum": 197
+            },
             "defense": 64,
             "baseExperience": 1033,
             "jobExperience": 605,
--- api/unit_test.py
+++ api/unit_test.py
@@ -8,10 +8,10 @@ import os
 @pytest.fixture(scope="module", autouse=True)
 def start_mongodb_container():
     client = docker.from_env()
-    client.volumes.create("mongodb")
     script_path = os.path.abspath("./mongodb_init_testdata.js")
     container = client.containers.run(
-        "mongo",
+        "mongo:latest",
+        name="mongo",
         detach=True,
         ports={"27017": ('0.0.0.0', 27017)},
         environment={
@@ -23,8 +23,12 @@ def start_mongodb_container():
                 "bind": "/docker-entrypoint-initdb.d/mongo-init.js",
                 "mode": "ro"
             },
-            "mongodb": {
+            "mongodb_data": {
                 "bind": "/data/db",
+                "mode": "rw"
+            },
+            "mongodb_config": {
+                "bind": "/data/configdb",
                 "mode": "rw"
             }
         }
@@ -34,23 +38,26 @@ def start_mongodb_container():
 
     container.stop()
     container.remove()
-    client.volumes.get("mongodb").remove(force=True)
+    client.volumes.get("mongodb_data").remove(force=True)
+    client.volumes.get("mongodb_config").remove(force=True)
 
 @pytest.fixture(scope="module")
 def test_client():
     client = TestClient(app)
     yield client
 
-def test_home_route(test_client):
-    response = test_client.get("/")
-    assert response.status_code == 200
-    assert "<title>Monsters</title>" in response.text
-
 def test_get_monsters(test_client):
-    response = test_client.get("/api/monsters?name=scorpion")
+    response = test_client.get("/api/monsters?name=esco")
     assert response.status_code == 200
     data = response.json()
     assert isinstance(data, list)
+    assert len(data) == 2
+
+    response = test_client.get("/api/monsters?name=rei")
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data, list)
+    assert len(data) == 1
 
 def test_get_monster(test_client):
     response = test_client.get("/api/monster/1001")
@@ -58,9 +65,67 @@ def test_get_monster(test_client):
     data = response.json()
     assert "name" in data
     assert "image_url" in data
+    assert data["id"] == 1001
 
 def test_get_nonexistent_monster(test_client):
     response = test_client.get("/api/monster/9999999999")
     assert response.status_code == 404
     data = response.json()
     assert 'error' in data
+
+def test_get_monsters_per_page(test_client):
+    for per_page in range(2):
+        response = test_client.get(f"/api/monsters?per_page={per_page}")
+        if per_page <= 0:
+            # A 422 status code indicates that the server was unable to process
+            # the request because it contains invalid data
+            assert response.status_code == 422
+        else:
+            assert response.status_code == 200
+            data = response.json()
+            assert len(data) == per_page
+
+def test_get_monsters_page(test_client):
+    response = test_client.get("/api/monsters?per_page=1&page=1")
+    assert response.status_code == 200
+    data = response.json()
+    assert data[0]['id'] == 1001
+
+    response = test_client.get("/api/monsters?per_page=1&page=2")
+    assert response.status_code == 200
+    data = response.json()
+    assert data[0]['id'] == 1168
+
+    response = test_client.get("/api/monsters?per_page=1&page=0")
+    assert response.status_code == 422
+
+    response = test_client.get("/api/monsters?per_page=1&page=-1")
+    assert response.status_code == 422
+
+    response = test_client.get("/api/monsters?per_page=1&page=string")
+    assert response.status_code == 422
+
+def test_get_healthy(test_client):
+    response = test_client.get("/api/health")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["healthy"] == "true"
+
+def test_get_monsters_race(test_client):
+    response = test_client.get("/api/monsters?race=human")
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) == 1
+    assert data[0]["stats"]["race"] == 7
+
+    response = test_client.get("/api/monsters?race=human,insect")
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) == 2
+    assert data[1]["stats"]["race"] == 7
+    assert data[0]["stats"]["race"] == 4
+
+    response = test_client.get("/api/monsters?race=inexistent")
+    assert response.status_code == 422
+    data = response.json()
+    assert data["detail"] == "race not exist: inexistent"
